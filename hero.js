2155eb7c7256bf694bb2c43c427d5b9031fac3fa
Alex Coulcher <alex.coulcher@ogilvy.com>
2014-10-08 09:38:21 +0100

    Testing

--- hero.js
+++ hero.js
@@ -79,7 +79,7 @@
 //   }
 // };
 
-// // The "Safe Diamond Miner"
+
 var move = function(gameData, helpers) {
   var myHero = gameData.activeHero;
 
@@ -93,13 +93,14 @@ var move = function(gameData, helpers) {
   var directionToHealthWell = healthWellStats.direction;
   
 
-  if (myHero.health < 40) {
-    //Heal no matter what if low health
-    return directionToHealthWell;
-  } else if (myHero.health < 100 && distanceToHealthWell === 1) {
+  if (myHero.health < 20) {
+    return helpers.findNearestEnemy(gameData)
+  }
+  else if (myHero.health < 100 && distanceToHealthWell === 1) {
     //Heal if you aren't full health and are close to a health well already
     return directionToHealthWell;
-  } else {
+  }
+  else {
     //If healthy, go capture a diamond mine!
     return helpers.findNearestNonTeamDiamondMine(gameData);
   }
